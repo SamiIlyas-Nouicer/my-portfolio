937c5b99e3614412ccf5437db124cedd18c95fab
SamiIlyas-nouicer <thesamii99@gmail.com>
2024-06-24 19:49:21 +0100

    d

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import outer from "../assets/outer-space.png"
 export default function About() {
   return (
     <div className="flex flex-col justify-center items-center m-4 sm:m-6 md:m-8 lg:m-10">
-      <div className="gradient-border-blur p-4 sm:p-5 md:p-6 lg:p-4 w-full lg:w-8/12 my-10 sm:my-12 md:my-14 lg:my-16">
+      <div className="gradient-border-blur p-4 sm:p-5 md:p-6 lg:p-4 w-full lg:w-8/12 my-10 sm:my-12 md:my-12 lg:my-12">
         <div className="inner-container w-full flex flex-col lg:flex-row items-center lg:items-start">
           <div className="w-full lg:w-6/12 flex flex-col justify-center mb-6 lg:mb-0">
             <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white mb-6 sm:mb-8 md:mb-10 text-center lg:text-left">
